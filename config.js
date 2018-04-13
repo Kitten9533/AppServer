b54ed275307c19ec612ab64e8755b22c1551f50f
jiangjiahao <1173860556@qq.com>
2018-04-13 17:10:18 +0800

    调用github api获取 token

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+	"clientId": "d0b4ef9184235d781fe5",
+	"clientSecret": "一楼不给看"
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,24 @@
+var router = require('./router');
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({
+	extended: true
+})); // for parsing application/x-www-form-urlencoded
+// app.use(multer()); // for parsing multipart/form-data
+
+//访问的文件放在虚拟目录 static下
+app.use('/static', express.static('public'));
+
+app.use('/app', router);
+
+var server = app.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('app listening at http://%s:%s', host, port);
+});
